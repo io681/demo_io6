401bbc18e40f0d8846f8ac0cea82a556e080a6ae
io681 <io-68@yandex.ru>
2024-05-03 12:37:49 +0300

    added checkstyle, packages in build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,7 @@
 plugins {
+	application
 	java
+	id("checkstyle")
 	id("org.springframework.boot") version "3.2.5"
 	id("io.spring.dependency-management") version "1.1.4"
 }
@@ -10,6 +12,7 @@ version = "0.0.1-SNAPSHOT"
 java {
 	sourceCompatibility = JavaVersion.VERSION_21
 }
+application { mainClass.set("ru.io6.demo.DemoApplication") }
 
 repositories {
 	mavenCentral()
@@ -17,6 +20,9 @@ repositories {
 
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter")
+	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("org.springframework.boot:spring-boot-devtools")
+
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
---
+++ src/main/java/ru/io6/demo/controller/WelcomeController.java
@@ -0,0 +1,11 @@
+package ru.io6.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
+public class WelcomeController {
+    @GetMapping(path = "/welcome")
+    public final String welcome() {
+        return "Welcome to Spring";
+    }
+}
