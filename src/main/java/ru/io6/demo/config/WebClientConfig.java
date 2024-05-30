d44d704e46db2c7ba586ca39444affdcf3e1f17f
io681 <io-68@yandex.ru>
2024-05-30 17:52:12 +0300

    fix checkstyle

--- src/main/java/ru/io6/demo/config/WebClientConfig.java
+++ src/main/java/ru/io6/demo/config/WebClientConfig.java
@@ -4,7 +4,6 @@ import io.netty.channel.ChannelOption;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 import org.springframework.core.env.Environment;
 import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.web.reactive.function.client.WebClient;
